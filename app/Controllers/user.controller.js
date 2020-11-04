538baf82734be8f204df982e87a2a3d423f5ac05
Chaitanya Maurya <mauryachaitany@gmail.com>
2020-11-04 10:10:39 +0530

    Expose following endpoints:
    1- Get all channels
    2- Get a specific channel
    3- Create a channel
    4- Create a user
    5- Get a user
    6- Get users in a channel
    7- Create message
    8- Get messages in a channel

--- app/Controllers/user.controller.js
+++ app/Controllers/user.controller.js
@@ -1,8 +1,7 @@
 const db = require('../connection')
 
-const users = db.users
-const channels = db.channels
-    // const paticipants = db.participants
+const Users = db.users
+const Channels = db.channels
 const Op = db.Sequelize.Op
 
 exports.create = async(req, res) => {
@@ -13,8 +12,8 @@ exports.create = async(req, res) => {
         return;
     }
     try {
-        const user = await users.create({ name: req.body.name, email: req.body.email, password: req.body.password })
-        const welcomeChannel = await channels.findOne({ where: { name: 'Welcome' } })
+        const user = await Users.create({ name: req.body.name, email: req.body.email, password: req.body.password })
+        const welcomeChannel = await Channels.findOne({ where: { name: 'Welcome' } })
         user.addChannels(welcomeChannel)
         res.send(user)
     } catch (err) {
@@ -24,17 +23,24 @@ exports.create = async(req, res) => {
         })
     }
 }
+
+exports.getUser = async(req, res) => {
+    const name = req.query.userName
+    const user = await Users.findOne({ where: { name } })
+    res.send(user)
+}
+
 exports.findUsersWithinChannel = async(req, res) => {
     const channelName = req.query.channelName
     const userID = req.query.userID
 
     try {
-        const user = await users.findByPk(userID)
-        const channel = await channels.findOne({ attributes: ['id'], where: { name: channelName } })
+        const user = await Users.findByPk(userID)
+        const channel = await Channels.findOne({ attributes: ['id'], where: { name: channelName } })
         await user.addChannels(channel)
 
-        const availableChannels = await channels.findAll({
-            include: [users],
+        const availableChannels = await Channels.findAll({
+            include: [Users],
             where: { name: channelName }
         })
         res.send(availableChannels)
--- app/Routes/user.routes.js
+++ app/Routes/user.routes.js
@@ -1,5 +1,8 @@
 module.exports = app => {
     const users = require("../Controllers/user.controller.js")
+
+    app.get('/user/', users.getUser)
+
     app.get('/users/', users.findUsersWithinChannel)
 
     app.post('/users/', users.create)
