5bf22d866ff1df91b3577ddc32471dbd4e9798c9
Chaitanya Maurya <mauryachaitany@gmail.com>
2020-11-04 16:56:19 +0530

    Use JOI for params validation

--- app/Controllers/message.controller.js
+++ app/Controllers/message.controller.js
@@ -1,3 +1,5 @@
+const Joi = require('joi')
+
 const db = require('../connection')
 
 const Messages = db.messages
@@ -16,24 +18,40 @@ exports.create = (req, res) => {
 }
 
 exports.findMessagesWithinChannel = async(req, res) => {
-    const channel = await Channels.findByPk(req.params.channelID)
-    const channelID = channel.toJSON()
-    let channelMessages = await Messages.findAll({ attributes: ['user_id', 'message'], where: { channel_id: channelID.id } })
-    const userIDs = channelMessages.reduce((acc, curr) => {
-        acc.push(curr.user_id)
-        return acc
-    }, [])
-    let users = await Users.findAll({ attributes: ['id', 'name'], where: { id: userIDs } })
-    users = users.reduce((acc, curr) => {
-        curr = curr.dataValues
-        acc.push(curr)
-        return acc
-    }, [])
-    const data = users.reduce((acc, curr) => {
-        let message = channelMessages.find(el => el.dataValues.user_id === curr.id) //Returns whole msg object
-        message = message.dataValues.message
-        acc.push({ user_id: curr.id, name: curr.name, message })
-        return acc
-    }, [])
-    res.send(data)
+    const channelIDScehma = Joi.object({
+        channelID: Joi.number().positive().integer()
+    })
+    const { error } = await channelIDScehma.validate({ channelID: req.params.channelID })
+    try {
+        if (error) {
+            const errMSg = "Invalid channelID"
+            throw new Error(errMSg)
+        }
+        const channel = await Channels.findByPk(req.params.channelID)
+        const channelID = channel.toJSON()
+        let channelMessages = await Messages.findAll({ attributes: ['user_id', 'message'], where: { channel_id: channelID.id } })
+        const userIDs = channelMessages.reduce((acc, curr) => {
+            acc.push(curr.user_id)
+            return acc
+        }, [])
+        let users = await Users.findAll({ attributes: ['id', 'name'], where: { id: userIDs } })
+        users = users.reduce((acc, curr) => {
+            curr = curr.dataValues
+            acc.push(curr)
+            return acc
+        }, [])
+        const data = users.reduce((acc, curr) => {
+            let message = channelMessages.find(el => el.dataValues.user_id === curr.id) //Returns whole msg object
+            message = message.dataValues.message
+            acc.push({ user_id: curr.id, name: curr.name, message })
+            return acc
+        }, [])
+        res.send(data)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send({
+            message: err.message || "Some error occurred while searching for messages"
+
+        })
+    }
 }
